5f7d01e512fe6801eb59639b4b93ee0ba3d35203
landerwells <landerwells@gmail.com>
2023-11-03 15:35:23 -0400

    make changes to template

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './music_player.js',
+  './music_player_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,9 @@ fn main() -> eframe::Result<()> {
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "Music Player",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(music_player::TemplateApp::new(cc))),
     )
 }
 
