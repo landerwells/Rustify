e39e9a019ed4029511e00f9d2c1ac2f6f2af392e
landerwells <landerwells@gmail.com>
2024-03-14 18:38:12 -0400

    Fixed progress bar to bugless state

--- README.md
+++ README.md
@@ -6,3 +6,21 @@
 
 Need to work on the play pause functionality paired with the track progress bar
 as there are some bugs making them desync
+
+
+all of the methods that will have to interact with the track progress states
+
+
+play
+playsong
+pause
+setprogress
+getprogress
+
+
+
+current goals,
+
+refactor the audio thread
+split into seperate files if it helps
+
--- src/audio_thread.rs
+++ src/audio_thread.rs
@@ -7,7 +7,6 @@ use std::fs::File;
 use std::io::BufReader;
 use std::sync::mpsc::{self, Sender};
 use std::thread;
-use std::thread::sleep;
 use std::time::Duration;
 use std::time::Instant;
 
@@ -41,44 +40,41 @@ pub fn create_audio_thread() -> Sender<AudioCommand> {
             let sink: Sink = rodio::Sink::try_new(&stream_handle).unwrap();
             let mut source: Decoder<BufReader<File>>;
             let mut start_time: Option<Instant> = None;
+            let mut last_pause_time: Option<Instant> = None;
             let mut track_duration: Option<Duration> = None;
-            let mut last_pause_time: Option<Duration> = None;
-            let mut elapsed_before_pause = Duration::ZERO; // Track total elapsed time before the last pause
-
 
             for command in receiver {
                 match command {
-                    AudioCommand::GetTrackDuration(sender) => {
-                        if current_state == AudioState::Empty {
-                            let _ = sender.send(Duration::from_secs(1));
-                            continue;
-                        }
-                        let _ = sender.send(track_duration.unwrap());
-                    }
+
                     AudioCommand::GetProgress(sender) => {
-                        // if the time the track has been playing exceeds the 
-                        // duration of the track, stop the track
-                        if current_state == AudioState::Playing {
-                            if let Some(track_duration) = track_duration {
-                                if start_time.unwrap().elapsed() > track_duration {
-                                    sink.stop();
-                                    current_state = AudioState::Empty;
-                                }
-                            }
-                        }
-                        let elapsed_time = match current_state {
-                            AudioState::Playing => {
-                                // If playing, calculate the elapsed time since the start or since the last resume
-                                let elapsed_since_start = start_time.unwrap_or_else(Instant::now).elapsed();
-                                elapsed_before_pause + elapsed_since_start // Add the time before the last pause
+                        // Debugging:
+                        // println!("Current state: {:?}", current_state);
+                        let progress = match current_state {
+                            AudioState::Empty => {
+                                Duration::from_secs(0)
                             },
-                            AudioState::Paused | AudioState::Empty => {
-                                // When paused or stopped, keep returning the time at the moment of the last pause
-                                elapsed_before_pause
+                            AudioState::Paused => {
+                                // If paused, calculate progress up to the pause time
+                                last_pause_time.map_or(Duration::from_secs(0), |pause_time| {
+                                    start_time.map_or(Duration::from_secs(0), |start| pause_time.duration_since(start))
+                                })
+                            },
+                            AudioState::Playing => {
+                                start_time.map_or(Duration::from_secs(0), |start| start.elapsed())
                             },
                         };
-                        let _ = sender.send(elapsed_time);
+
+                        // Debugging:
+                        // println!("Progress: {:?}", progress);
+                        let _ = sender.send(progress);
+
+                        // Reset last_pause_time to None when playing
+                        if current_state == AudioState::Playing {
+                            last_pause_time = None;
+                        }
                     },
+
+                    // this method might need some more work to be production ready
                     AudioCommand::SetProgress(progress, file_path) => {
                         sink.stop();
                         let file = match File::open(&file_path) {
@@ -103,59 +99,76 @@ pub fn create_audio_thread() -> Sender<AudioCommand> {
                             // Adjust start_time to reflect the progress point without resuming playback.
                             // This ensures that when we resume, we do so from the correct point.
                             start_time = Some(Instant::now() - skip_duration);
-                            // Since we're paused, we update elapsed_before_pause to reflect the current progress.
-                            elapsed_before_pause = skip_duration;
                         }
                     },
-                    AudioCommand::Play => {
-                        if current_state != AudioState::Playing {
-                            sink.play();
-                            current_state = AudioState::Playing;
-                            start_time = Some(Instant::now()); // Reset start_time on play if not already playing
-                        }
-                        println!("The audio state is currently: {:?}", current_state);
-                    },
-                        AudioCommand::PlaySong(file_path) => {
-                            let file = match File::open(&file_path) {
-                                Ok(f) => BufReader::new(f),
-                                Err(e) => {
-                                    eprintln!("Error opening file: {}", e);
-                                    continue;
-                                }
-                            };
-                            println!("{}", file_path);
 
-                            // Since we're only dealing with .wav files, directly attempt to decode as wav
-                            source = Decoder::new_wav(file).unwrap();
-                            track_duration = Some(source.total_duration().unwrap());
-                            sink.stop();
-                            sleep(Duration::from_millis(200));
-                            sink.append(source);
-                            sink.play();
-                            current_state = AudioState::Playing;
+                    AudioCommand::Play => {
+                        // Calculate the total paused duration if there's a last_pause_time.
+                        // This assumes `last_pause_time` represents the moment the latest pause began,
+                        // and `elapsed_before_pause` accumulates all pause durations before the latest one.
+                        if let Some(last_pause) = last_pause_time {
+                            let pause_duration = Instant::now().duration_since(last_pause);
+                            // Adjust the start_time by the total pause duration (including the most recent pause).
+                            // This essentially "moves" the start_time forward by pause_duration, shortening the total elapsed time by the paused duration.
+                            start_time = Some(start_time.unwrap_or(Instant::now()) + pause_duration);
+                        } else {
+                            // If there's no pause (e.g., starting playback for the first time), initialize start_time to now.
                             start_time = Some(Instant::now());
-                            // Reset elapsed_before_pause to 0 when starting a new track
-                            elapsed_before_pause = Duration::ZERO;
-                                 
-                            println!("The audio state is currently: {:?}", current_state);
                         }
+
+                        // Reset last_pause_time as playback is resuming.
+                        last_pause_time = None;
+
+                        // Start or resume playback.
+                        sink.play();
+
+                        // Update current state to Playing.
+                        current_state = AudioState::Playing;
+                    },
+
+                    AudioCommand::PlaySong(file_path) => {
+                        // extract out into seperate method
+                        let file = match File::open(&file_path) {
+                            Ok(f) => BufReader::new(f),
+                            Err(e) => {
+                                eprintln!("Error opening file: {}", e);
+                                continue;
+                            }
+                        };
+
+                        source = Decoder::new_wav(file).unwrap();
+                        track_duration = Some(source.total_duration().unwrap());
+                        sink.clear();
+                        sink.append(source);
+                        sink.play();
+                        start_time = Some(Instant::now());
+                        last_pause_time = None; 
+                        current_state = AudioState::Playing;
+                    }
+
                     AudioCommand::Pause => {
                         sink.pause();
                         current_state = AudioState::Paused;
-                        // Update elapsed time at the moment of pause
-                        elapsed_before_pause += start_time.unwrap_or_else(Instant::now).elapsed();
+                        last_pause_time = Some(Instant::now());
                         println!("The audio state is currently: {:?}", current_state);
                     },
+                    // Below this nothing should need to be changed
                     AudioCommand::Skip => {
                         if sink.len() <= 1 {
                             sink.stop();
                             current_state = AudioState::Empty;
-                            println!("The audio state is currently: {:?}", current_state);
                         } else {
                             sink.skip_one();
                             current_state = AudioState::Playing;
-                            println!("The audio state is currently: {:?}", current_state);
                         }
+                        println!("The audio state is currently: {:?}", current_state);
+                    }
+                    AudioCommand::GetTrackDuration(sender) => {
+                        if current_state == AudioState::Empty {
+                            let _ = sender.send(Duration::from_secs(1));
+                            continue;
+                        }
+                        let _ = sender.send(track_duration.unwrap());
                     }
                     AudioCommand::GetState(sender) => {
                         if sink.empty() {
